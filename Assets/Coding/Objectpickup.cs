10d1b53bb8349f73e82cb4283834716cf0df785e
Nameless653 <namelessguy653@gmail.com>
2023-02-15 13:24:53 +1100

    Full Update Week 1
    added alot to make the game function

--- Assets/Coding/Objectpickup.cs
+++ Assets/Coding/Objectpickup.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Objectpickup : MonoBehaviour
-{ 
+{
+    private int score;
+    public TextMeshProGUI scoreText;
 
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
 
     void OnTriggerEnter(Collider collision)
     {
@@ -13,6 +21,8 @@ public class Objectpickup : MonoBehaviour
             Debug.Log("Collision Has occured");
             Destroy(collision.gameObject);
             Debug.Log("Book has been picked up");
+            score = ++1;
+            Debug.Log("Score has been increased");
         }
     }
 }
